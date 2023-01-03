86083cbd2a3f30889d3da2516c9dbc9dbab1b5f1
test3teamapp <test3teamapp@gmail.com>
2023-01-03 15:24:11 +0200

    track-router.gs can now handle requests for readding the stream of previous location updates within a range of past hours.

--- routers/track-router.js
+++ routers/track-router.js
@@ -10,18 +10,41 @@ export const router = Router()
 router.get('/byName/:name/hours/:hours', async (req, res) => {
     const name = req.params.name
     const hours = Number(req.params.hours)
-  
+
     const person = await personRepository.search().where('name').equals(name).return.first()
-  
+
     //console.log(JSON.stringify(person))
     if (person == null) {
-      //console.log("ERROR :" + `NO PERSON FOUND BY NAME: ${name}`)
-      res.send({ "ERROR": `NO PERSON FOUND BY NAME: ${name}` })
+        //console.log("ERROR :" + `NO PERSON FOUND BY NAME: ${name}`)
+        res.send({ "ERROR": `NO PERSON FOUND BY NAME: ${name}` })
     } else {
-  
-      let keyName = `${person.keyName}:locationHistory`
-      const jsonObj = await redis.xrange(keyName, "-", "+", "COUNT", 1)
-  
-      res.send(jsonObj)
+
+        let keyName = `${person.keyName}:locationHistory`
+        // first get the latest entry in the stream, to establish the range of milliseconds to request
+        const jsonObj = await redis.xrevrange(keyName, "+", "-", "COUNT", 1)
+
+        /*
+        We will receive something like this:
+        [
+            [
+            "1672482216758-0",
+                [
+                "longitude",
+                "38.2333814",
+                "latitude",
+                "21.7266567"
+                ]
+            ]
+        ]
+        this is the latest timestamp in ms : 1672482216758
+        We need to transform hours in ms and substract from the above to get the range of entries requested
+        */
+        //console.log(`Latest timestamp : ${jsonObj[0][0]}`)
+        //console.log(`Latest locations : ${jsonObj[0][1]}`)
+        const latestTimestamp = Number(String(jsonObj[0][0]).substring(0,13))
+        //console.log(`Latest timestamp as number: ${latestTimestamp}`)
+        const pastTimestamp = latestTimestamp - (hours * 3600000)
+        const tracks = await redis.xrange(keyName, pastTimestamp, latestTimestamp)
+        res.send({ name, tracks})
     }
-  })
+})
