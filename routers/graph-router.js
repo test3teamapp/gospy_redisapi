e1922f2612c814a195612555a6334f3e1ebcd977
test3teamapp <test3teamapp@gmail.com>
2023-01-13 16:31:55 +0000

    friends' connections api call also added

--- routers/graph-router.js
+++ routers/graph-router.js
@@ -8,7 +8,7 @@ const redis = new Redis();
 
 export const router = Router()
 
-
+// latest meeting by name
 router.get('/latest/byName/:name/hours/:hours', async (req, res) => {
     const name = req.params.name
     const hours = Number(req.params.hours)
@@ -21,9 +21,8 @@ router.get('/latest/byName/:name/hours/:hours', async (req, res) => {
         res.send({ "ERROR": `NO PERSON FOUND BY NAME: ${name}` })
     } else {
 
-        // check if there are any other meetings taking place in the area already
         let timestampNow = Date.now();
-        const timestamp1HourAgo = timestampNow - (hours * 60 * 60000); // past hour
+        const timestampAgo = timestampNow - (hours * 60 * 60000); // past hour(s)
 
         const graphResult = await meetingsGraph.roQuery(
             'MATCH (m:Meeting) , (pl:Place) , (allp:Person ), (p:Person {name: $name}) \
@@ -33,11 +32,113 @@ router.get('/latest/byName/:name/hours/:hours', async (req, res) => {
             {
                 params: {
                     name: name,
-                    tPastHour: timestamp1HourAgo
+                    tPastHour: timestampAgo
                 }
             }
         );
         res.send({ name, graphResult })
 
+    }
+})
+
+// places user had meetings with counters
+router.get('/count/places/byName/:name/months/:months', async (req, res) => {
+    const name = req.params.name
+    const months = Number(req.params.months)
+
+    const person = await personRepository.search().where('name').equals(name).return.first()
+
+    //console.log(JSON.stringify(person))
+    if (person == null) {
+        //console.log("ERROR :" + `NO PERSON FOUND BY NAME: ${name}`)
+        res.send({ "ERROR": `NO PERSON FOUND BY NAME: ${name}` })
+    } else {
+
+        let timestampNow = Date.now();
+        const timestampAgo = timestampNow - (months * 30 * 24 * 60 * 60000); // past month(s)
+
+        const graphResult = await meetingsGraph.roQuery(
+            'MATCH (m:Meeting) , (pl:Place) , (p:Person {name: $name}) \
+  WHERE (m)-[:AT_PLACE]->(pl)  AND (p)-[:PART_OF]->(m)\
+  AND m.date > $tPastHour RETURN pl as place, COUNT(m) as placeCount ORDER BY placeCount DESC',
+            {
+                params: {
+                    name: name,
+                    tPastHour: timestampAgo
+                }
+            }
+        );
+        res.send({ name, graphResult })
+
+    }
+})
+
+// friend of friend of user 
+router.get('/fof/byName/:name', async (req, res) => {
+    const name = req.params.name
+
+    const person = await personRepository.search().where('name').equals(name).return.first()
+    var tempFriendSet = [];
+
+    //console.log(JSON.stringify(person))
+    if (person == null) {
+        //console.log("ERROR :" + `NO PERSON FOUND BY NAME: ${name}`)
+        res.send({ "ERROR": `NO PERSON FOUND BY NAME: ${name}` })
+    } else {
+
+        var graphResult = await meetingsGraph.roQuery(
+            'MATCH (p:Person {name: $name})-[:PART_OF]->(m1:Meeting)<-[:PART_OF]-(f:Person) \
+            WHERE (f.name <> $name) RETURN DISTINCT f.name as friend',
+            {
+                params: {
+                    name: name
+                }
+            }
+        );
+        if (graphResult.ERROR) { // if there was an error, we just return the error
+            // not the result we expected
+            console.log(" outer.get('/fof/byName/:" + name + ": Response of 1st call : " + graphResult.ERROR);
+            res.send({ name, graphResult })
+        } else {
+            // we have a set of first level connections (could be 0 if the person has no meetings)
+
+            if (graphResult.data.length == 0) {
+                res.send({ name, graphResult });
+            } else {
+                graphResult.data.forEach(f => {
+                    tempFriendSet.push(f.friend);
+                });
+                console.log(" outer.get('/fof/byName/:" + name + ":Friends list : " + tempFriendSet);
+
+                graphResult = await meetingsGraph.roQuery(
+                    'UNWIND $friendsList as friend\
+                         MATCH (p:Person {name: friend})-[:PART_OF]->(m1:Meeting)<-[:PART_OF]-(fof:Person) \
+                         WHERE (fof.name <> $name) AND NOT ANY( f IN $friendsList WHERE (fof.name = f)) \
+                         RETURN DISTINCT friend, fof.name as fof',
+                    {
+                        params: {
+                            friendsList: tempFriendSet,
+                            name: name
+                        }
+                    }
+                );
+                console.log(" outer.get('/fof/byName/:" + name + ":Response of 2nd call : " + JSON.stringify(graphResult));
+
+                if (!graphResult.data) { // if there was an error, we just return the error
+                    // not the result we expected
+                    //console.log(" outer.get('/fof/byName/:name': Response of 2nd call : " + graphResult);
+                    res.send({ name, ERROR: 'NO RESULT FOR FRIENDS OF FRIENDS' });
+                } else if (graphResult.ERROR) { // if there was an error, we just return the error
+                    // not the result we expected
+                    //console.log(" outer.get('/fof/byName/:name': Response of 2nd call : " + graphResult.ERROR);
+                    res.send({ name, graphResult });
+                } else {
+                    graphResult['friends'] = tempFriendSet; 
+                    res.send({ name, graphResult });
+                }
+
+            }
+
+        }
     }
 })
